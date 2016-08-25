e22a025bc92d244e6ce49a69237b5359255bc44c
John Henriksson <jokkish@gmail.com>
2016-08-25 09:59:14 +0200

    fixed some styling

--- index.php
+++ index.php
@@ -2,93 +2,36 @@
 get_header ();
 
 ?>
-<!-- Main content -->
-<div class="row">
-	<!-- Posts -->
-	<div class="col s8 m8 l8">
-		<?php
-		if (have_posts()) :
-			while (have_posts()) : the_post();
-			?>
-		<?php
-		$thumb_id = get_post_thumbnail_id();
-		$thumb_url = wp_get_attachment_image_src($thumb_id, 'card-center', true);
-		 ?>
-      	<?php if (has_post_thumbnail()) : ?>
-	      <div class="card ">
-	      	<a href="<?php the_permalink(); ?>">
-	        <div class="card-image">
-				<img src="<?php echo $thumb_url[0]; ?>" class="responsive-img">
-				
-	          <!-- <span class="card-title"><h4><?php the_title();?></h4></span> -->
-	        </div>
-	        </a>
-	        <div class="card-content">
-	        <span class="card-title"><h5><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h5></span>
-	       <?php else : ?>
-	        <div class="card">
-	       	<div class="card-content">
-	          <span class="card-title"><h5><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h5></span>
-	    	<?php endif; ?>
 
-	          <p><?php the_excerpt(); ?></p>
-	        </div>
-	        <div class="card-action">
-	          <a href="<?php the_permalink(); ?>" class="btn btn-large" >Läs mer...</a>
-	        </div>
-	      </div>
-		<?php endwhile; ?>
-	</div>
-    <!-- End posts -->
-
-	<!-- Sidebar -->
-	<div class="col s4 m4">
-		<!-- Search -->
-		<div class="row">
-			<?php get_search_form(); ?>
-		</div>
-		<!-- End Search -->
-		<!-- Categories -->
-		<div class="row">
-			<h5>Kategorier</h5>
-			<div class="collection">
-				<?php 
-					$categories = get_categories();
-					$cur_category = get_category( get_query_var( 'cat' ) );
-					// $cur_cat_id = $cur_category->cat_ID;
-					
-					foreach ($categories as $category) {
-						if ($category == $cur_category) {
-							echo '<a class="collection-item active" href="'.get_category_link($category).'">'.$category->name."</a>";
-						}else{
-							echo '<a class="collection-item" href="'.get_category_link($category).'">'.$category->name."</a>";	
-						}
-						
-					 } 
-				?>		
-			</div>
-		</div>
-		<!-- End Categories -->
-	</div>
-	<!-- End sidebar -->
-</div>
-<!-- End Maint Content -->
-
-<!-- Pagination -->
-<div class="row">
-	<div class="col s6 m6 l6 pagination">
-		<h6 class="left-align btn btn-large"><?php previous_posts_link(); ?></h6>
-	</div>
-	<div class="col s6 m6 l6 pagination">
-		<h6 class="right-align btn btn-large"><?php next_posts_link(); ?></h6>
-	</div>
-</div>
-<!-- End Pagination -->
-
-
-
-
-	<?php
+<?php
+if (have_posts()) :
+	while (have_posts()) : the_post();
+	?>
+  <div class="post">
+  <h1 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+  <div class="post-meta">
+  <?php echo get_the_date(); ?> av <?php echo the_author_posts_link(); ?> -
+  <?php
+    $categories = get_the_category();
+    $separator = ' | ';
+    $output = '';
+    if ( ! empty( $categories ) ) {
+        foreach( $categories as $category ) {
+            $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
+        }
+        echo trim( $output, $separator );
+    }
+   ?>
+  </div>
+  <?php the_content( 'Läs mer av inlägget: ' .  get_the_title() ); ?>
+  </div>
+    <p>
+  This post currently has
+  <?php comments_number( 'no responses', 'one response', '% responses' ); ?>.
+</p>
+  <div class="row post-seperator">
+  </div>
+	<?php endwhile;
 
 	else :
 		echo '<p>No content found</p>';
--- single.php
+++ single.php
@@ -7,8 +7,22 @@ get_header ();
 if (have_posts()) :
 	while (have_posts()) : the_post();
 	?>
-  <div class="single-content">
-  <h1><?php the_title(); ?></h1>
+  <div class="post">
+  <h1 class="post-title"><?php the_title(); ?></h1>
+  <div class="post-meta">
+  <?php echo get_the_date(); ?> av <?php echo the_author_posts_link(); ?> -
+  <?php
+    $categories = get_the_category();
+    $separator = ' | ';
+    $output = '';
+    if ( ! empty( $categories ) ) {
+        foreach( $categories as $category ) {
+            $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
+        }
+        echo trim( $output, $separator );
+    }
+   ?>
+  </div>
   <?php the_content(); ?>
   </div>
 	<?php endwhile;
